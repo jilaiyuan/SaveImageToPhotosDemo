1d0babbf86d3e5631c76901aedbafadc63fdec43
Michael Yuan <michael.yuan@zoom.us>
2019-08-07 13:48:48 +0800

    Create demo project
    Create demo project

---
+++ SaveImageToPhotosDemo/SaveImageToPhotosDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SaveImageToPhotosDemo
+//
+//  Created by Admin on 2019/8/7.
+//  Copyright © 2019 com.personal.project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SaveImageToPhotosDemo/SaveImageToPhotosDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SaveImageToPhotosDemo
+//
+//  Created by Admin on 2019/8/7.
+//  Copyright © 2019 com.personal.project. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
